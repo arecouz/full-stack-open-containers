2a19b3ab76a431a8fb32080a8caa8d02473a1173
arecouz <richardcouzens96@gmail.com>
2024-09-26 11:24:04 +0100

    Exercise 12.15: Set up a frontend development environment

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -4,11 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001,
+    watch: {
+      usePolling: true,
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './testSetup.js',
+    watch: false,
   },
 });
-
-
