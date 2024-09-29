14c91dd3617d12cf1da91b9a74097eecb2c45135
arecouz <richardcouzens96@gmail.com>
2024-09-29 17:14:08 +0100

    Exercises 12.17. - 12.19. (nginx reverse proxy server)

--- todo-app/todo-frontend/dev.Dockerfile
+++ todo-app/todo-frontend/dev.Dockerfile
@@ -1,5 +1,4 @@
 FROM node:20
-ENV VITE_BACKEND_URL=http://localhost:3000/
 
 WORKDIR /usr/src/app
 
--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
     watch: {
       usePolling: true,
     },
